b5be2e0a3b04b52fd4b4afec2e34f91ec78d8426
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-07 01:03:36 +0300

    Update OptiBestConfig.java

--- src/main/java/com/optibest/config/OptiBestConfig.java
+++ src/main/java/com/optibest/config/OptiBestConfig.java
@@ -41,4 +41,8 @@ public class OptiBestConfig {
     public static boolean soundThrottle = true;
     public static boolean hudOptimization = true;
     public static boolean itemRenderCache = true;
+    public static boolean mapRenderOff = true;
+    public static boolean noEntityShadow = true;
+    public static boolean reducedMipmaps = true;
+    public static boolean skipOffscreenBlockEntities = true;
 }
