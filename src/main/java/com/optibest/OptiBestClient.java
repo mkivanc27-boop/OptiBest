3316508e2eb0970399768a95974cdb779fbb57c4
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-06 22:24:28 +0300

    Update OptiBestClient.java

--- src/main/java/com/optibest/OptiBestClient.java
+++ src/main/java/com/optibest/OptiBestClient.java
@@ -3,7 +3,6 @@ package com.optibest;
 import com.optibest.config.OptiBestConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.CloudRenderMode;
 import net.minecraft.client.option.GraphicsMode;
 import net.minecraft.client.option.ParticlesMode;
@@ -16,43 +15,36 @@ public class OptiBestClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        LOGGER.info("§6[OptiBest] §aYüklendi! Tüm optimizasyonlar aktif.");
+        LOGGER.info("[OptiBest] Yüklendi!");
 
         ClientTickEvents.START_CLIENT_TICK.register(client -> {
             if (client.options == null) return;
 
-            // FPS sınırsız
-            if (OptiBestConfig.unlimitedFps) {
+            if (OptiBestConfig.unlimitedFps)
                 client.options.getMaxFps().setValue(260);
-            }
 
-            // Render mesafesi sabit 4
             client.options.getViewDistance().setValue(OptiBestConfig.targetViewDistance);
 
-            // Bulutlar
-            if (OptiBestConfig.cloudRenderOff) {
+            if (OptiBestConfig.cloudRenderOff)
                 client.options.getCloudRenderMode().setValue(CloudRenderMode.OFF);
-            }
 
-            // Entity gölgeleri
-            if (OptiBestConfig.entityShadowsDisabled) {
+            if (OptiBestConfig.entityShadowsDisabled)
                 client.options.getEntityShadows().setValue(false);
-            }
 
-            // Grafik modu
-            if (OptiBestConfig.fastGraphics) {
+            if (OptiBestConfig.fastGraphics)
                 client.options.getGraphicsMode().setValue(GraphicsMode.FAST);
-            }
 
-            // Smooth lighting
-            if (OptiBestConfig.smoothLightingOff) {
+            if (OptiBestConfig.smoothLightingOff)
                 client.options.getAo().setValue(false);
-            }
 
-            // Partikül modu
-            if (OptiBestConfig.particleLimit) {
+            if (OptiBestConfig.particleLimit)
                 client.options.getParticles().setValue(ParticlesMode.MINIMAL);
-            }
+
+            // Entity görüş mesafesi %50
+            client.options.getEntityDistanceScaling().setValue(0.5f);
+
+            // Mipmap sıfır
+            client.options.getMipmapLevels().setValue(0);
         });
     }
 }
